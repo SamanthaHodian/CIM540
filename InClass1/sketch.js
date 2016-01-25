1073dcf7068e1d00d9c8bfe8c453bb9f5cc8fe35
SamanthaHodian <samanthahodian@Samanthas-MacBook-Pro.local>
2016-01-25 14:07:17 -0500

    debug 1

---
+++ InClass1/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+    background(12,12,230);
+    noStroke();
+    fill(100);
+    rect(30,200,70,300);
+    rect(130,275,100,225)
+  
+}
