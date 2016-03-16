2632ac42ad72fb3ec038e13ec869cc727ebda21d
SamanthaHodian <samanthahodian@Samanthas-MacBook-Pro.local>
2016-03-16 14:34:57 -0400

    particles

---
+++ inClass7/sketch.js
@@ -0,0 +1,44 @@
+
+
+var Particle = function(x,y){
+ this.x=x;
+ this.y=y;
+ this.diameter = random(1,8);
+ this.yspeed = random (-4,4);
+  this.xspeed = random (-2,2);
+ this.show = function() {
+   ellipse(this.x,this.y, this.diameter, this.diameter);
+ }
+ this.move = function() {
+   this.y = this.y + this.yspeed;
+   this.x= this.x + this.xspeed;
+  }
+}
+var p = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  background (0);
+  fill (random(0,255), random(0,255), random(255));
+  for (var i = 0; i < p.length; i++){
+    p[i].show();
+    p[i].move();
+  }
+
+}
+
+function mouseDragged(){
+  p.push(new Particle (random(0,windowWidth), random(0,windowHeight)));
+}
+
+function mousePressed(){
+  p.push(new Particle (mouseX, mouseY));
+}
+
+function keyPressed () {
+  p[i].fall();
+}
